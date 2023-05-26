636e70020a57a8461e46b68c060f2c4ac950ba3e
Jake152 <jakepeterson1233@gmail.com>
2023-05-25 22:56:03 -0500

    Initial page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import PageLayout from './PageLayout';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="weather-app">
-      <header className="weather-app-header">
-        <span>Weather App</span>
-        <div id='github-info-container'>
-          <span>Creator Github: </span>
-          <a
-            className="github-link"
-            href="https://github.com/Jake152"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            github.com/Jake152
-          </a>
-        </div>
-      </header>
+    <div id="weather-app">
+      <PageLayout />
     </div>
   );
 }
---
+++ src/PageLayout.jsx
@@ -0,0 +1,11 @@
+import './PageLayout.css';
+
+function PageLayout() {
+  return (
+    <div id="weather-app-page-layout">
+      Weather App Initial Layout
+    </div>
+  );
+}
+
+export default PageLayout;
