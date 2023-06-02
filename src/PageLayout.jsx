6128522eed30deeca7b5c99eca15f2461faf76e4
Jake152 <jakepeterson1233@gmail.com>
2023-06-02 16:30:35 -0500

    Workitem: Pagelayout styling and implmenting current geo location including session/local storage

--- src/PageLayout.jsx
+++ src/PageLayout.jsx
@@ -1,26 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage/HomePage';
-import TodayPage from './Pages/TodayPage/TodayPage';
 import WeatherHeader from './Components/WeatherHeader/WeatherHeader';
 import WeatherFooter from './Components/WeatherFooter/WeatherFooter';
+import LocationPermissionModal from './Components/LocationPermissionModal/LocationPermissionModal';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './PageLayout.css';
 
 export default function PageLayout() {
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [latitude, setLatitude] = useState(localStorage.getItem('latitude') || '');
+  const [longitude, setLongitude] = useState(localStorage.getItem('longitude') || '');
+  const [currentGeolocationAccess, setCurrentGeolocationAccess] = useState(
+    // localStorage.getItem('currentGeolocationAccess') === 'true' || false
+    sessionStorage.getItem('currentGeolocationAccess') === 'true' || false
+  );
+  const [showModal, setShowModal] = useState(
+    // localStorage.getItem('currentGeolocationAccess') !== 'true'
+    sessionStorage.getItem('currentGeolocationAccess') !== 'true'
+  );
+
+  useEffect(() => {
+    // localStorage.setItem('currentGeolocationAccess', currentGeolocationAccess);
+    sessionStorage.setItem('currentGeolocationAccess', currentGeolocationAccess);
+  }, [currentGeolocationAccess]);
+
+  useEffect(() => {
+    // localStorage.setItem('showModal', showModal);
+    sessionStorage.setItem('showModal', showModal);
+  }, [showModal]);
+
+  const handleAllowLocationAccess = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+  
+          // Save latitude and longitude to localStorage
+          // localStorage.setItem('latitude', latitude);
+          sessionStorage.setItem('latitude', latitude);
+          // localStorage.setItem('longitude', longitude);
+          sessionStorage.setItem('longitude', longitude);
+  
+          setLatitude(latitude);
+          setLongitude(longitude);
+          setCurrentGeolocationAccess(true);
+          setShowModal(false);
+        },
+        (error) => {
+          console.error(error);
+          // Handle any errors that occur while getting the user's location
+        }
+      );
+    } else {
+      console.log("Geo location is not supported by the browser");
+    }
+  };
+
+  const handleDenyLocationAccess = () => {
+    setLatitude('44.6879693');
+    setLongitude('-93.2137714');
+    setCurrentGeolocationAccess(false);
+    setShowModal(false);
+  };
+
   return (
     <Router>
       <div id='page-layout'>
-          <WeatherHeader />
+        <WeatherHeader />
           <main id='main-content-container'>
             <div id='main-content'>
-              <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/today' element={<TodayPage />} />
-              </Routes>
+              {!showModal && (
+                <Routes>
+                  <Route path='/' element={<HomePage latitude={latitude} longitude={longitude} city={city} state={state} />} />
+                </Routes>
+              )}
             </div>
           </main>
           <WeatherFooter />
+          <LocationPermissionModal showModal={showModal} handleAllowLocationAccess={handleAllowLocationAccess} handleDenyLocationAccess={handleDenyLocationAccess} />
       </div>
     </Router>
   );
