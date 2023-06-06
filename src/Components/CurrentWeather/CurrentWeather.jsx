828837d75126b69581fa2a68e533ef99110c2776
Jake152 <jakepeterson1233@gmail.com>
2023-06-05 22:28:33 -0500

    Workitem: Implementation of new data retrieval and new sections for periods of day data and current conditions

--- src/Components/CurrentWeather/CurrentWeather.jsx
+++ src/Components/CurrentWeather/CurrentWeather.jsx
@@ -9,13 +9,13 @@ export default function CurrentWeather({ latitude, longitude }) {
     const [locationData, setLocationData] = useState(null);
     const [currentWeatherData, setCurrentWeatherData] = useState(null);
     const [oneDayWeatherData, setOneDayWeatherData] = useState(null);
-    const [forecastData, setForecastData] = useState(null);
+    const [forecastPeriodData, setForecastPeriodData] = useState(null);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
 
     const renderCurrentTempSection = () => (
         <>
-            {locationData && currentWeatherData && (
+            {locationData && currentWeatherData && oneDayWeatherData && (
                 <div id="current-temp-container">
                     <div id="location-info">
                         {locationData?.LocalizedName}, {locationData?.AdministrativeArea.LocalizedName} as of {convertToTime(currentWeatherData?.LocalObservationDateTime)} {locationData?.TimeZone.Code}
@@ -26,33 +26,91 @@ export default function CurrentWeather({ latitude, longitude }) {
                     <div id="weather-conditions">
                         {currentWeatherData?.WeatherText}
                     </div>
-                    <div id="day-night-temp">
-                        High {oneDayWeatherData?.DailyForecasts[0].Temperature.Maximum.Value}° - Low {oneDayWeatherData?.DailyForecasts[0].Temperature.Minimum.Value}°
+                    <div id="high-low-temp">
+                        High {oneDayWeatherData?.Temperature.Maximum.Value}° - Low {oneDayWeatherData?.Temperature.Minimum.Value}°
                     </div>
                 </div>
             )}
         </>
     );
 
+    const renderTodayWeatherInfo = () => (
+      <>
+        {locationData && currentWeatherData && oneDayWeatherData && (
+          <div id="today-weather-container">
+            <div id="title-row">
+              <span id="forecast-title">Weather Today in {locationData?.LocalizedName}, {locationData?.AdministrativeArea.ID}</span>
+            </div>
+            <div id="temperature-info">
+                <span id="current-temp-text">{currentWeatherData?.RealFeelTemperature.Imperial.Value}°</span>
+                <span id="feels-like-text">Feels Like</span>
+            </div>
+            <div id="additional-info">
+                <div id="high-low-temp" className="additional-info-item">
+                  <span className="label">High / Low</span> 
+                  <span className="data-text">{oneDayWeatherData?.Temperature.Maximum.Value}° / {oneDayWeatherData?.Temperature.Minimum.Value}°</span>
+                </div>
+                <div id="wind-speed-info" className="additional-info-item">
+                  <span className="label">Wind</span> 
+                  <span className="data-text">{currentWeatherData?.Wind.Speed.Imperial.Value} MPH</span>
+                </div>
+                <div id="dew-point-info" className="additional-info-item">
+                  <span className="label">Dew Point</span> 
+                  <span className="data-text">{currentWeatherData?.DewPoint.Imperial.Value}°</span>
+                </div>
+                <div id="wind-direction-info" className="additional-info-item">
+                  <span className="label">Wind Direction</span> 
+                  <span className="data-text">{currentWeatherData?.Wind.Direction.Localized}</span>
+                </div>
+                <div id="humidity-info" className="additional-info-item">
+                  <span className="label">Relative Humidity</span> 
+                  <span className="data-text">{currentWeatherData?.RelativeHumidity}%</span>
+                </div>
+                <div id="wind-gusts-info" className="additional-info-item">
+                  <span className="label">Wind Gust</span> 
+                  <span className="data-text">{currentWeatherData?.WindGust.Speed.Imperial.Value} MPH</span>
+                </div>
+                <div id="cloud-cover-info" className="additional-info-item">
+                  <span className="label">Cloud Cover</span> 
+                  <span className="data-text">{currentWeatherData?.CloudCover}%</span>
+                </div>
+                <div id="visibility-info" className="additional-info-item">
+                  <span className="label">Visibility</span> 
+                  <span className="data-text">{currentWeatherData?.Visibility.Imperial.Value} {currentWeatherData?.Visibility.Imperial.Unit}</span>
+                </div>
+                <div id="pressure-info" className="additional-info-item">
+                  <span className="label">Pressure</span> 
+                  <span className="data-text">{currentWeatherData?.Pressure.Imperial.Value} {currentWeatherData?.Pressure.Imperial.Unit}</span>
+                </div>
+                <div id="uvindex-info" className="additional-info-item">
+                  <span className="label">UV Index</span> 
+                  <span className="data-text">{currentWeatherData?.UVIndex} {currentWeatherData?.UVIndexText}</span>
+                </div>
+            </div>
+          </div>
+        )}
+      </>
+    );
+
     const renderForecastSection = () => (
       <>
-        {forecastData && locationData && (
+        {forecastPeriodData && locationData && (
           <div id="forecast-container">
             <div id="title-row">
               <span id="forecast-title">Today's Forecast for {locationData?.LocalizedName}, {locationData?.AdministrativeArea.ID}</span>
             </div>
             <div id="values-row">
               <div className="forecast-item" id="morning-temp">
-                <span className="label">Morning</span> <span className="value">{forecastData?.morning}°</span>
+                <span className="label">Morning</span> <span className="value">{forecastPeriodData?.morning.Temperature.Value}°</span>
               </div>
               <div className="forecast-item" id="afternoon-temp">
-                <span className="label">Afternoon</span> <span className="value">{forecastData?.afternoon}°</span>
+                <span className="label">Afternoon</span> <span className="value">{forecastPeriodData?.afternoon.Temperature.Value}°</span>
               </div>
               <div className="forecast-item" id="evening-temp">
-                <span className="label">Evening</span> <span className="value">{forecastData?.evening}°</span>
+                <span className="label">Evening</span> <span className="value">{forecastPeriodData?.evening.Temperature.Value}°</span>
               </div>
               <div className="forecast-item" id="overnight-temp">
-                <span className="label">Overnight</span> <span className="value">{forecastData?.overnight}°</span>
+                <span className="label">Overnight</span> <span className="value">{forecastPeriodData?.overnight.Temperature.Value}°</span>
               </div>
             </div>
           </div>
@@ -65,6 +123,22 @@ export default function CurrentWeather({ latitude, longitude }) {
         <span id="loading-data-message">Loading data...</span>
         <Spinner id="loading-spinner" animation="border" />
       </div>
+    );
+
+    const renderWrappers = () => (
+      <>
+        {locationData && currentWeatherData && oneDayWeatherData && forecastPeriodData && (
+          <div id="current-weather-container">
+            <div className="current-temp-wrapper">
+              {renderCurrentTempSection()}
+              {renderTodayWeatherInfo()}
+            </div>
+            <div className="forecast-wrapper">
+              {renderForecastSection()}
+            </div>
+          </div>
+        )}
+      </>
     );
 
     const fetchWeatherData = async () => {
@@ -82,7 +156,7 @@ export default function CurrentWeather({ latitude, longitude }) {
           if (currentWeather && oneDayWeather && forecastData) {
             setCurrentWeatherData(currentWeather);
             setOneDayWeatherData(oneDayWeather);
-            setForecastData(forecastData);
+            setForecastPeriodData(forecastData);
           } else {
             setError('Error fetching weather data');
           }
@@ -102,8 +176,8 @@ export default function CurrentWeather({ latitude, longitude }) {
         console.log('Location Key', locationData?.Key)
         console.log('Current Weather Data', currentWeatherData);
         console.log('One Day Weather', oneDayWeatherData);
-        console.log('Forecast Data', forecastData);
-    }, [locationData, currentWeatherData, forecastData]);
+        console.log('Forecast Data', forecastPeriodData);
+    }, [locationData, currentWeatherData, forecastPeriodData]);
 
     useEffect(() => {
       let isMounted = true;
@@ -117,23 +191,9 @@ export default function CurrentWeather({ latitude, longitude }) {
     }, []);
 
     return (
-        <div id="current-weather-container">
-            {error && <div id="error-message">{error}</div>}
-            {isLoading ? (
-              renderLoadingSpinner()
-            ) : (
-              // renderCurrentTempSection()
-              <>
-                {/* First column */}
-                <div className="column" id="current-temp-column">
-                  {renderCurrentTempSection()}
-                </div>
-                {/* Second column */}
-                <div className="column" id="forecast-column">
-                  {renderForecastSection()}
-                </div>
-              </>
-            )}
-        </div>
-    )
+      <>
+        {error && <div id="error-message">{error}</div>}
+        {isLoading ? renderLoadingSpinner() : renderWrappers()}
+      </>
+    );
 }
