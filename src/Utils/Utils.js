00a384b769bc767ad78904d6d51c36fbb57e0e03
Jake152 <jakepeterson1233@gmail.com>
2023-06-10 19:29:06 -0500

    Workitem: util file with logic to getWeather Icon and change time format

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,3 +1,5 @@
+import WeatherIcons from "../Assets/WeatherIcons/WeatherIcons";
+
 export function getCurrentTime() {
     const currentDate = new Date();
     const options = {
@@ -22,3 +24,69 @@ export function convertToTime(timestamp) {
     const formattedTime = date.toLocaleString('en-US', options);
     return formattedTime;
   }
+
+const checkDayTime = (hour, sunrise, sunset) => {
+  const parseHour = (hourString) => {
+    const [hourValue, period] = hourString.split(' ');
+    let hourNumber = parseInt(hourValue, 10);
+    if (period === 'PM' && hourNumber !== 12) {
+      hourNumber += 12;
+    } else if (period === 'AM' && hourNumber === 12) {
+      hourNumber = 0;
+    }
+    return hourNumber;
+  };
+
+  const sunriseTime = parseHour(sunrise);
+  const sunsetTime = parseHour(sunset);
+  const currentHour = parseHour(hour);
+
+  if (currentHour > sunriseTime && currentHour < sunsetTime) {
+    return 'day';
+  } else {
+    return 'night';
+  }
+}
+
+
+export function getWeatherIcon(condition, hour, sunrise, sunset) {
+  const dayOrNight = checkDayTime(hour, sunrise, sunset);
+  if (dayOrNight === 'day') {
+    switch (condition) {
+      case 'Clear':
+      case 'Mostly clear':
+        return WeatherIcons.Clear_MostlyClear;
+      case 'Partly sunny':
+      case 'Partly cloudy':
+      case 'Intermittent clouds':
+        return WeatherIcons.PartlyCloudy;
+      case 'Cloudy':
+      case 'Mostly cloudy':
+        return WeatherIcons.Cloudy;
+      case 'Showers':
+        return WeatherIcons.Rain;
+      case 'Partly sunny w/ t-storms':
+      case 'Thunderstorms':
+        return WeatherIcons.Thunderstorm;
+      default:
+    }
+  }
+
+  switch (condition) {
+    case 'Clear':
+    case 'Mostly clear':
+      return WeatherIcons.Clear_MostlyClear_NIGHT;
+    case 'Partly cloudy':
+    case 'Intermittent clouds':
+      return WeatherIcons.PartlyCloudy_NIGHT;
+    case 'Cloudy':
+    case 'Mostly cloudy':
+      return WeatherIcons.Cloudy;
+    case 'Showers':
+      return WeatherIcons.Rain;
+    case 'Partly sunny w/ t-storms':
+    case 'Thunderstorms':
+      return WeatherIcons.Thunderstorm;
+    default:
+  }
+}
