725bd0e34b2f1feace2bebc0a2d23a15824440d4
Jake152 <jakepeterson1233@gmail.com>
2023-06-10 19:28:44 -0500

    Workitem: Adding Weather Icons pngs and util file to use them

---
+++ src/Assets/WeatherIcons/WeatherIcons.js
@@ -0,0 +1,37 @@
+import Clear_MostlyClear_NIGHT from './Clear_MostlyClear_NIGHT.png';
+import Clear_MostlyClear from './Clear_MostlyClear.png';
+import Cloudy from './Cloudy.png';
+import Drizzle_FreezingDrizzle from './Drizzle_FreezingDrizzle.png';
+import Drizzle_NIGHT from './Drizzle_NIGHT.png';
+import Fog from './Fog.png';
+import Haze from './Haze.png';
+import HeavyRain from './HeavyRain.png';
+import HeavySnow_Sleet_WintryMix from './HeavySnow_Sleet_WintryMix.png';
+import PartlyCloudy_NIGHT from './PartlyCloudy_NIGHT.png';
+import PartlyCloudy from './PartlyCloudy.png';
+import Rain from './Rain.png';
+import Snow from './Snow.png';
+import Sunrise from './Sunrise.png';
+import Sunset from './Sunset.png';
+import Thunderstorm from './Thunderstorm.png';
+import Windy_Breezy from './Windy_Breezy.png';
+
+export default {
+  Clear_MostlyClear_NIGHT,
+  Clear_MostlyClear,
+  Cloudy,
+  Drizzle_FreezingDrizzle,
+  Drizzle_NIGHT,
+  Fog,
+  Haze,
+  HeavyRain,
+  HeavySnow_Sleet_WintryMix,
+  PartlyCloudy_NIGHT,
+  PartlyCloudy,
+  Rain,
+  Snow,
+  Sunrise,
+  Sunset,
+  Thunderstorm,
+  Windy_Breezy,
+};
