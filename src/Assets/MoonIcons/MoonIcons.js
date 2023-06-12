109cd3905df5a879ea9c24ab5e943dabe7758ad5
Jake152 <jakepeterson1233@gmail.com>
2023-06-11 21:28:00 -0500

    Workitem: adding moon phase to the moonIcons

--- src/Assets/MoonIcons/MoonIcons.js
+++ src/Assets/MoonIcons/MoonIcons.js
@@ -1,5 +1,7 @@
 import Moon_Last_Quarter from './Moon_Last_Quarter.png';
+import Waning_Crescent_Moon from './Waning_Crescent_Moon.png';
 
 export default {
     Moon_Last_Quarter,
+    Waning_Crescent_Moon,
 };
