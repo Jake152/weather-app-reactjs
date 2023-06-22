0a98c01adedf3fb30dc66f46046267d93cb11ab8
Jake152 <jakepeterson1233@gmail.com>
2023-06-21 19:39:20 -0500

    Workitem: Adding TodayPage component to Today Page

--- src/Pages/TodayPage/TodayPage.jsx
+++ src/Pages/TodayPage/TodayPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
+import CurrentWeather from '../../Components/CurrentWeather/CurrentWeather';
 
 import './TodayPage.css';
 
-export default function TodayPage() {
+export default function TodayPage({ latitude, longitude }) {
+
     return (
         <div id="today-page-container">
-            <span>Weather App Today Page</span>
+            <CurrentWeather latitude={latitude} longitude={longitude} />
         </div>
     )
 }
